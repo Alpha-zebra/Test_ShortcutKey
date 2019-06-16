0858af867a1eb419f4dc28189c5645935d9e0b22
Administrator <1983402481@qq.com>
2019-06-16 14:57:41 +0800

    test git

---
+++ src/A.java
@@ -0,0 +1,57 @@
+public class A {
+
+
+    public static void a() {
+        System.out.println("aa");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+        a();
+        a();
+
+    }
+}
